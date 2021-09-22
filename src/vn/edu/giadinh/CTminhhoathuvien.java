4f29664ac3c4a6040200e95b96ca99591eca1da7
DSTDevils <nguyendinhnam121002@gmail.com>
2021-09-22 23:18:53 +0700

    lưu khôi lần đầu tiên

---
+++ src/vn/edu/giadinh/CTminhhoathuvien.java
@@ -0,0 +1,15 @@
+package vn.edu.giadinh;
+
+public class CTminhhoathuvien {
+    
+    public static void main(String[] args){
+        int songuyen = 7;
+        double ketQua;
+
+        ketQua = Math.sqrt(songuyen);
+
+        System.out.println("ket qua tinh can " + ketQua);
+        System.out.printf("%.2f", ketQua);
+    }
+
+}
