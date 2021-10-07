11cc7f9f1b6ac698a690ddfeb349746a21dfddb9
DSTDevils <nguyendinhnam121002@gmail.com>
2021-10-07 07:45:51 +0700

    sửa

---
+++ src/com/namnd2008110152/lab3/lab3bai2.java
@@ -0,0 +1,14 @@
+package com.namnd2008110152.lab3;
+
+public class lab3bai2 {
+    public static void main(String[] args) {
+        for(int i=2;i<=9;i++){
+            System.out.println();
+            for(int j=1;j<=9;j++){
+                System.out.printf("%d x %d = %d", i, j, i*j);
+                System.out.println();
+            }
+        }
+    }
+    
+}
