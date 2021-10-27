d22d0974af254db14d1c5c77d967a86a12b3827b
DSTDevils <90500823+DSTDevils@users.noreply.github.com>
2021-10-27 22:25:32 +0700

    Add files via upload

---
+++ src/com/namnd2008110152/tuan7/Caterpillar.java
@@ -0,0 +1,20 @@
+/*
+* created date:27 Oct 2021
+* author: cgm
+*/
+package com.namnd2008110152.tuan7;
+
+public class Caterpillar {
+    void eat(){
+        System.out.println("plant");
+    }
+    void sleep(){
+        System.out.println("zzz");
+    }
+    void walk(){
+        System.out.println("finding food");
+    }
+    void evolution(){
+        System.out.println("butterfly");
+    }
+}
