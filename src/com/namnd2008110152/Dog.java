bc1d610a6edb5815df832b95bf293d12a43602fb
DSTDevils <90500823+DSTDevils@users.noreply.github.com>
2021-10-27 22:21:36 +0700

    Add files via upload

---
+++ src/com/namnd2008110152/Dog.java
@@ -0,0 +1,27 @@
+/*
+* created date:21 Oct 2021
+* author: cgm
+*/
+package com.namnd2008110152.tuan7;
+
+public class Dog {
+    void makenoise(){
+        System.out.println("bark bark...");
+    }
+    void eat(){
+        System.out.println("dog food");
+    }
+    void sleep(){
+        System.out.println("zzz");
+    }
+    void walk(){
+        System.out.println("canh nha");
+    }
+    void bite(){
+        System.out.println("protect the master");
+    }
+    void chase(){
+        System.out.println("chasing the cat");
+    }
+
+}
---
+++ src/com/namnd2008110152/Wolf.java
@@ -0,0 +1,24 @@
+/*
+* created date:27 Oct 2021
+* author: cgm
+*/
+package com.namnd2008110152.tuan7;
+
+    public class Wolf {
+        void makenoise(){
+            System.out.println("grú");
+        }
+        void eat(){
+            System.out.println("meat"); 
+        }
+        void sleep(){
+            System.out.println("zzz"); 
+        }
+        void hunting(){
+            System.out.println("hunt by crew"); 
+        }
+        void bite(){
+            System.out.println("protect the crew");
+        }
+    }
+
