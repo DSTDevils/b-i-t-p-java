a020ee4523df56f5013482615ebc9ad729f86286
DSTDevils <90500823+DSTDevils@users.noreply.github.com>
2021-11-03 21:42:42 +0700

    Add files via upload

---
+++ src/com/namnd2008110152/lap8/bai1/SanPhamTestDrive.java
@@ -0,0 +1,41 @@
+/*
+* created date:03 Nov 2021
+* author: cgm
+*/
+package com.namnd2008110152.lap8.sanpham;
+
+
+    import java.util.ArrayList;
+
+import java.util.Scanner;
+
+public class SanPhamTestDrive {
+            private static Scanner in;
+
+            public static void main(String[] args) {
+                SanPham sp = new SanPham();
+                SanPham sp1 = new SanPham();
+                SanPham sp2 = new SanPham();
+                sp.Nhap();
+                sp1.Nhap();
+                sp2.Nhap();
+                sp.Xuat();
+                sp1.Xuat();
+                sp2.Xuat();
+                int n;
+        ArrayList<SanPham> list = new ArrayList<SanPham>();
+        System.out.print("Nhap so luong phan tu: ");
+        n = in.nextInt();
+        for(int i=0;i<n;i++){
+            SanPham sanpham = new SanPham();
+            sp.Nhap();
+            list.add(sp);
+        }
+        System.out.println(" thong tin cac san pham trong list");
+        for(SanPham sPham: list){
+            sPham.Xuat();
+        }
+    }
+}
+
+   
