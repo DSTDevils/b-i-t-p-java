7977035295be61c728ba9815762b5a0af961cb50
DSTDevils <90500823+DSTDevils@users.noreply.github.com>
2021-11-03 21:48:40 +0700

    Add files via upload

---
+++ src/com/namnd2008110152/lap8/bai2/HinhChuNhat.java
@@ -0,0 +1,33 @@
+package com.namnd2008110152.lap8.shape;
+
+public class HinhChuNhat {
+    public class ChuNhat {
+        private double a;
+        private double b;
+    
+        public ChuNhat(double a, double b) {
+            this.a = a;
+            this.b = b;
+        }
+        public void xuat() {
+            System.out.println("Chieu dai: "+a);
+            System.out.println("Chieu rong: "+ b);
+            System.out.println("Chu vi: ("+ a +"+"+b+")*2 = "+getChuVi());
+            System.out.println("Dien tich: "+a +"*"+b+"= "+getDienTich());
+        }
+        protected double getChuVi() {
+            return (a+b)*2;
+        }
+        protected double getDienTich() {
+            return a*b;
+        }
+        protected double geta() {
+            return a;
+        }
+        protected double getb() {
+            return b;
+        }
+    }
+
+    public void xuat() {
+    }
---
+++ src/com/namnd2008110152/lap8/bai2/ShapeTestDrive.java
@@ -0,0 +1,16 @@
+/*
+* created date:03 Nov 2021
+* author: cgm
+*/
+package com.namnd2008110152.lap8.shape;
+
+public class ShapeTestDrive {
+        public static void main(String[] args) {
+            HinhChuNhat hinhchunhat = new HinhChuNhat();
+            hinhchunhat.xuat();
+    
+            HinhVuong hinhvuong = new HinhVuong();
+            hinhvuong.xuat();
+        }
+    }
+
